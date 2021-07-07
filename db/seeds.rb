6184844cd392b40e177019097282deffb7cb248f
jssgajendrasingh <mailmegajendra14@gmail.com>
2021-07-07 14:38:09 +0530

    generating the faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+	user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email,  about: Faker::Lorem.paragraphs, password: "123456")
+	article = Article.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraphs, is_publish: [true, false].sample, user: user)
+	tag = Tag.create(name: Faker::Lorem.sentence(word_count: 3))
+	article_tag = ArticleTag.create(tag: tag,article: article)
+	comment = Comment.create(fullname: Faker::Name.name,email: Faker::Internet.email,message: Faker::Lorem.paragraphs,status: [true,false].sample,article: article)
+	message = Message.create(fullname: Faker::Name.name,email: Faker::Internet.email,content: Faker::Lorem.paragraphs)
+  setting = Setting.create(blog_name: Faker::Artist.name,post_per_page: Faker::Number.number(digits: 2),if_maintance: [true,false].sample)
+  #notification = Notification.create(notifiable: [message,comment].sample,notifiable_type: )
+end	
