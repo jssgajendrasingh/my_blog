67ea1557876f22a94a0be48ac62cfa7fe27fd9cc
jssgajendrasingh <mailmegajendra14@gmail.com>
2021-07-07 18:01:50 +0530

    adding namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # namespace :admin do
+  # 	resources :users do
+  # 		resources :articles do 
+  # 			resources :comments
+  # 		end	
+  # 	end	
+  # end
+  namespace :admin do
+  	resources :users, :articles,:comments
+  end
 end
